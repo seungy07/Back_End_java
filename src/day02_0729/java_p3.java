7ad4e3059890d49709bafbdcf8956868dc4b2683
seungy07 <ksykim07@gmail.com>
2026-07-30 10:45:17 +0900

    p3 반영

--- src/day02_0729/java_p3.java
+++ src/day02_0729/java_p3.java
@@ -37,93 +37,51 @@ public class java_p3 {
         System.out.print("비밀번호:"); String pw = scanner.next();
         System.out.println(id.equals("admin") && pw.equals("1234")); // .equals()
 
-    /*[문제 7] Scanner를 이용해 정수 하나를 입력받아, 그 수가 홀수이면서 7의 배수이면 true, 아니면 false를 출력하시오.
-    입력 예시:
-    정수를 입력하세요: 21
-    출력 예시:
-    결과: true */
+    /*[문제 7] Scanner를 이용해 정수 하나를 입력받아, 그 수가 홀수이면서 7의 배수이면 true, 아니면 false를 출력하시오.*/
+        System.out.print("정수 하나를 입력:"); int 정수하나 = scanner.nextInt();
+        System.out.println(정수하나 % 7 == 0 && 정수하나 % 2 == 1);
 
+    /*[문제 8] Scanner를 이용해 1차 점수와 2차 점수를 각각 int로 입력받아, 두 점수의 총점이 150점 이상이면 "합격", 아니면 "불합격"을 출력하시오. */
+        System.out.print("1차점수 입력:"); int 점수1 = scanner.nextInt();
+        System.out.print("2차점수 입력:"); int 점수2 = scanner.nextInt();
+        System.out.println((점수1 + 점수2) >= 150 ? "합격": "불합격");
 
-    /*[문제 8] Scanner를 이용해 1차 점수와 2차 점수를 각각 int로 입력받아, 두 점수의 총점이 150점 이상이면 "합격", 아니면 "불합격"을 출력하시오.
-    입력 예시:
-    1차 점수: 80
-    2차 점수: 75
-    출력 예시:
-    결과: 합격 */
+    /*[문제 9]  Scanner를 이용해 이름을 String으로 입력받아, 만약 이름이 "유재석"과 일치하면 이름 뒤에 "(방장)"을 붙여서 출력하고, 아니면 입력받은 이름만 출력하시오.*/
+        System.out.print("9번 문제 이름을 입력해:"); String 이름 = scanner.next();
+        System.out.println((이름.equals("유재석") ? 이름+"(방장)" : 이름));
 
+    /*[문제 10] Scanner를 이용해 삼각형의 밑변과 높이를 각각 double로 입력받아, 삼각형의 넓이를 계산하여 출력하시오.*/
+        System.out.print("10번 삼각형의 밑변: "); double 밑변 = scanner.nextDouble();
+        System.out.print("10번 삼각형의 높이: "); double 높이 = scanner.nextDouble();
+        System.out.println("삼각형의 넓이:" +(밑변 * 높이)/2.0); 
 
-    /*[문제 9]  Scanner를 이용해 이름을 String으로 입력받아, 만약 이름이 "유재석"과 일치하면 이름 뒤에 "(방장)"을 붙여서 출력하고, 아니면 입력받은 이름만 출력하시오.
-    입력 예시 1:
-    이름을 입력하세요: 유재석
-    출력 예시 1:
-    유재석(방장)
-    입력 예시 2:
-    이름을 입력하세요: 강호동
-    출력 예시 2:
-    강호동 */
+    /*[문제 11] Scanner를 이용해 섭씨 온도를 double로 입력받아, 화씨 온도로 변환하여 출력하시오.*/
+        System.out.print("11번 섭씨온도 : "); double 섭씨온도 = scanner.nextDouble();
+        System.out.println("화씨 온도: " +((섭씨온도*9.0/5.0)+32));
 
+    /*[문제 12] Scanner를 이용해 태어난 년도(int)를 입력받아, 2025년 기준 현재 나이를 계산하여 출력하시오.*/
+        System.out.print("12번 태어난 년도 : "); int 탄생년도 = scanner.nextInt();
+        System.out.println("2025년 기준 나이: "+(2025-탄생년도)+"세");
 
-    /*[문제 10] Scanner를 이용해 삼각형의 밑변과 높이를 각각 double로 입력받아, 삼각형의 넓이를 계산하여 출력하시오.
-    요구 조건: 공식: 밑변 * 높이 / 2.0
-    입력 예시:
-    밑변: 10.0
-    높이: 5.5
-    출력 예시:
-    삼각형의 넓이: 27.5
+    /*[문제 13] Scanner를 이용해 키(cm)와 몸무게(kg)를 각각 double로 입력받아, BMI 지수를 계산하여 출력하시오.*/
+        System.out.print("13번 키cm : "); double 키 = scanner.nextDouble();
+        System.out.print("13번 몸무게kg : "); double 몸무게 = scanner.nextDouble();
+        System.out.println("BMI 지수: " +(몸무게/(키/100.0 * 키/100.0)));
+        
+    /*[문제 14] Scanner를 이용해 아이디와 이메일을 String으로 입력받습니다. 
+    아이디가 "admin"이거나 또는 이메일이 "admin@test.com"이면 "관리자", 그렇지 않으면 "일반 사용자"를 출력하시오.*/
+        System.out.print("14번 Id : "); String 아이디 = scanner.next();
+        System.out.print("14번 이메일 : "); String 이메일 = scanner.next();
+        System.out.println(아이디.equals("admin") || 이메일.equals("admin@test.com") ? "관리자":"일반 사용자");
 
+    /*[문제 15] Scanner를 이용해 100점 만점의 점수(int)를 입력받아, 점수에 따라 아래와 같이 등급을 부여하고 출력하시오.*/
+        System.out.print("15번  점수3입력 100점 만점 : "); int 점수3  = scanner.nextInt();
+        System.out.println(점수3 >= 90 ? 'A': 점수3 < 90 && 점수3 >= 80 ? 'B': 점수3 < 70 ? 'C': "점수다시 입력");
 
-    [문제 11] Scanner를 이용해 섭씨 온도를 double로 입력받아, 화씨 온도로 변환하여 출력하시오.
-    요구 조건:
-    공식: (섭씨 * 9.0 / 5.0) + 32
-    입력 예시:
-    섭씨 온도: 30.0
-    출력 예시:
-    화씨 온도: 86.0 */
+    /*[문제 16] Scanner를 이용해 나이(int)를 입력받아, 20대(20세 이상 29세 이하)에 해당하는 경우 "이벤트 대상입니다."를, 그렇지 않으면 "이벤트 대상이 아닙니다."를 출력하시오.*/
+        System.out.print("16번 나이 입력 : "); int 나이  = scanner.nextInt();
+        System.out.println(나이<=29 && 나이>=20 ? "이벤트 대상입니다.":"이벤트 대상이 아닙니다");
 
-
-    /*[문제 12] Scanner를 이용해 태어난 년도(int)를 입력받아, 2025년 기준 현재 나이를 계산하여 출력하시오.
-    요구 조건:
-    공식: 2025 - 태어난 년도
-    입력 예시:
-    태어난 년도: 1998
-    출력 예시:
-    2025년 기준 나이: 27세 */
-
-
-    /*[문제 13] Scanner를 이용해 키(cm)와 몸무게(kg)를 각각 double로 입력받아, BMI 지수를 계산하여 출력하시오.
-    요구 조건:
-    BMI 공식: 몸무게(kg) / (키(m) * 키(m))
-    키는 cm로 입력받지만, 공식에는 m 단위가 사용되므로 키(cm) / 100.0으로 단위를 변환해야 합니다.
-    입력 예시:
-    키(cm): 175.5
-    몸무게(kg): 68.8
-    출력 예시:
-    BMI 지수: 22.34... */
-
-
-    /*[문제 14] Scanner를 이용해 아이디와 이메일을 String으로 입력받습니다. 아이디가 "admin"이거나 또는 이메일이 "admin@test.com"이면 "관리자", 그렇지 않으면 "일반 사용자"를 출력하시오.
-    입력 예시:
-    아이디: user1
-    이메일: admin@test.com
-    출력 예시:
-    결과: 관리자 */
-
-
-    /*[문제 15] Scanner를 이용해 100점 만점의 점수(int)를 입력받아, 점수에 따라 아래와 같이 등급을 부여하고 출력하시오.
-    90점 이상: 'A'
-    80점 이상 90점 미만: 'B'
-    70점 미만: 'C'
-    입력 예시:
-    점수를 입력하세요: 88
-    출력 예시:
-    등급:B */
-
-
-    /*[문제 16] Scanner를 이용해 나이(int)를 입력받아, 20대(20세 이상 29세 이하)에 해당하는 경우 "이벤트 대상입니다."를, 그렇지 않으면 "이벤트 대상이 아닙니다."를 출력하시오.
-    입력 예시:
-    나이를 입력하세요: 25
-    출력 예시:
-    이벤트 대상입니다. */
     }
 
 }
