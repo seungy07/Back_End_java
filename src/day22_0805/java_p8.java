2c777e9a7a73ce10492015da6672128b8d97fc49
seungy07 <ksykim07@gmail.com>
2026-08-05 12:05:50 +0900

    실습 8번 풀이

--- src/day22_0805/java_p8.java
+++ src/day22_0805/java_p8.java
@@ -1,36 +1,65 @@
 package day22_0805;
 
+import java.util.Arrays;
+
+//[문제 1] Book 클래스를 만드세요. (title, author, price 멤버 변수)
+class Book{ // IDE(vscode) 소스 작업-> 자동 생성 기능 ( 자동생성자 ) constructor -> 멤버변수 체크 -> 확인
+    String title; String author; int price;
+    Book(String title, String author, int price){
+        this.title = title; this.author = author; this.price = price;}}
+
+// [문제 2] Rectangle 클래스를 만드세요. (width, height 멤버 변수) *멤버변수
+class Rectangle{
+    int width; int height;
+    Rectangle(int width, int height){
+        this.width=width; this.height=height;}}
+
+// [문제 3] BankAccount 클래스를 만드세요. (accountNumber, ownerName, balance 멤버 변수) *멤버변수
+class BankAccount{
+    String acconutNumber; String ownerName; int balance;
+    BankAccount(String acconutNumber, String ownerName, int balance){
+        this.acconutNumber=acconutNumber; this.ownerName=ownerName; this.balance=balance;}
+}
+// [문제 4] Goods 클래스를 만드세요. (name, price 멤버 변수) *멤버변수
+class Goods{
+    String name; int price;
+    Goods(){ price = 0;}
+    Goods(String name, int price){
+        this.name=name; this.price=price;
+    }
+}
+// [문제 5] Member 클래스를 만드세요. (id, isLogin 멤버 변수) *멤버변수
+class Member{
+    String id; boolean isLogin;
+    Member(){//id는 "guest", isLogin은 false로 초기화되도록
+        id = "guest"; isLogin = false;
+    }
+}
+
 public class java_p8 {
     public static void main(String[] args) {
-    
     //[문제 1] Book 클래스를 만드세요. (title, author, price 멤버 변수) *멤버변수의 타입은 적절하게 선택하시오.
-    // 1. Book 클래스에 title, author, price를 매개변수로 받아 멤버 변수를 초기화하는 생성자를 만드세요.
-    // 2. main 함수에서 이 생성자를 사용하여, ("이것이 자바다", "신용권", 30000) 정보와 ("자바의 정석", "남궁성", 28000)
-    // 정보를 가진 Book 객체 2개를 생성하고, 각 객체의 정보를 출력하세요.
+        Book book1 = new Book("이것이 자바다.", "신용권", 3000);
+        Book book2 = new Book("자바의 정석", "남궁성", 28000);
+        System.out.printf("%s %s %d", book1.title, book1.author, book1.price);
+        System.out.printf("\n%s %s %d", book2.title, book2.author, book2.price);
 
     // [문제 2] Rectangle 클래스를 만드세요. (width, height 멤버 변수) *멤버변수의 타입은 적절하게 선택하시오.
-    // 1. width와 height를 매개변수로 받아 초기화하는 생성자를 만드세요.
-    // 2. 사각형의 넓이(width * height)를 계산하여 반환하는 getArea() 메소드를 만드세요.
-    // 3. main 함수에서 (10, 5) 값을 가진 Rectangle 객체를 생성하고, getArea() 메소드를 호출하여 "사각형의 넓이: [넓
-    // 이]"를 출력하세요.
+        Rectangle rectangle = new Rectangle(10, 5);
+        System.out.printf("\n %d %d", rectangle.width, rectangle.height);
+        
 
-    // [문제 3] BankAccount 클래스를 만드세요. (accountNumber, ownerName, balance 멤버 변수) *멤버변수의
-    // 타입은 적절하게 선택하시오
-    // 1. 세 멤버 변수를 모두 초기화하는 생성자를 만드세요.
-    // 2. 입금(deposit)과 출금(withdraw) 기능을 하는 메소드를 각각 만드세요.
-    // 3. main 함수에서 ("111-222-3333", "유재석", 10000) 정보로 객체를 생성한 뒤, 5000원을 입금하고 3000원을 출금
-    // 한 후의 최종 잔액을 출력하세요.
+    // [문제 3] BankAccount 클래스 (다양한 데이터 타입을 가진 생성자
+        BankAccount bankAccount = new BankAccount("111-222-3333", "유재석", 10000);
+        System.out.printf("\n%s %s %d", bankAccount.acconutNumber,bankAccount.ownerName, bankAccount.balance);
 
     // [문제 4] Goods 클래스를 만드세요. (name, price 멤버 변수) *멤버변수의 타입은 적절하게 선택하시오.
-    // 1. Goods 클래스에 생성자를 두 개 만드세요(오버로딩).
-    // 2. 기본 생성자: name은 "미정", price는 0으로 초기화
-    // 3. 매개변수가 있는 생성자: name과 price를 매개변수로 받아 초기화
-    // 4. main 함수에서 기본 생성자로 객체 하나, 매개변수가 있는 생성자로 ("콜라", 2000) 정보를 가진 객체 하나를 각각 생성
-    // 하고, 두 객체의 정보를 모두 출력하세요.
+        Goods goods1 = new Goods(); Goods goods2 = new Goods("콜라",2000);
+        System.out.printf("\n%s %d", goods1.name, goods1.price);
+        System.out.printf("\n%s %d", goods2.name, goods2.price);
 
     // [문제 5] Member 클래스를 만드세요. (id, isLogin 멤버 변수) *멤버변수의 타입은 적절하게 선택하시오.
-    // 1. Member 클래스에 기본 생성자를 만들고, 이 생성자 안에서 id는 "guest", isLogin은 false로 초기화되도록 하세요.
-    // 2. main 함수에서 new Member()로 객체를 생성하고, 초기화된 id와 isLogin 값을 출력하여 확인하세요.*
+        Member member = new Member(); System.out.printf("\n%s %b" , member.id,member.isLogin);
     }
 
 }
