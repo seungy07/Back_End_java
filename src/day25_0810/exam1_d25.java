d92e75903b2ee88623e0f2a1473f41ca86b6bfa1
seungy07 <ksykim07@gmail.com>
2026-08-10 11:39:51 +0900

    자바 11문제 풀이

--- src/day25_0810/exam1_d25.java
+++ src/day25_0810/exam1_d25.java
@@ -1,5 +1,80 @@
 package day25_0810;
 
+import java.util.ArrayList;
+
 public class exam1_d25 {
+    public static void main(String[] args) {
+        /*
+            배열 : 여러개 (동일한 타입)자료들을 저장하는 타입, 정적길이
+            ArrayList 클래스 : 컬렉션(수집) 프레임(틀)워크(일), 자료 수집관련 클래스/기능 제공
+                    목적: 가변길이, 배열 관련된 기능(메소드) 제공한다. <편리하다>
+                사용법
+                    ArrayList<제너릭타입> 변수명 = new ArrayList<>();
+                    - 제너릭타입: 리스트내 저장할 요소들의 타입, 즉] <String> 리스트에 여러개 String 저장
+                    - 주의할점: <제너릭타입>은 기본타입이 불가능하다. int -> Integer 래퍼타입(기본 타입을 클래스타입 변환 사용)
+                                                                        -멤버변수,메소드 제공
+                주요메소드
+                    1. .add(자료) : 리스트 요소 추가
+                    2. .add(인덱스, 자료) : 인덱스 위치 삽입 (중간 삽입)
+                    3. .set(인덱스, 자료) : 인덱스 위치 값 수정
+                        - 리스트명.get(1) = "수정"  *불가  , 배열명[1] = "수정" ok
+                    4. .size( ) : 리스트내 요소 개수 반환
+                    5. .get(인덱스) : 인덱스 위치 요소 반환
+                    6. .remove(인덱스) : 인덱스위치 요소 삭제
+                    7. .clear( ) : 리스트내 요소 전체 삭제
+                    8. .indexOf( 찾을값 ) : 리스트내 존재하면 인덱스 반환, 없으면 -1 반환
+                    9. .contains( 찾을 값 ) : 값 존재하면 true ,  없으면 false
+                    10. .isEmpty( ) : 리스트 비어있으면 true , false
+
+        */      
+       // [1] 리스트 생성
+       ArrayList< String > 변수명1 = new ArrayList<>(); // String 타입 여러개 저장하는 리스트
+
+       // [2] 리스트 요소 추가
+       변수명1.add("유재석"); 변수명1.add("강호동"); 변수명1.add("신동엽");
+
+       // [3] 리스트 전체 출력
+       System.out.println(변수명1); // [유재석, 강호동, 신동엽 ]
+
+       // [4] 리스트 중간 삽입
+       변수명1.add( 1, "하하"); // ( 인덱스, 추가할값)
+       System.out.println(변수명1); 
+       
+       // [5] 리스트 요소 수정
+       변수명1.set(1, "서장훈");  // (인덱스, 수정값)
+       System.out.println(변수명1);
+
+       // [6] 리스트 요소 총 개수
+       변수명1.size();
+       System.out.println(변수명1.size());
+
+       // [7] 리스트 특정 요소 출력
+       변수명1.get(2); // (인덱스)  인덱스에 해당하는 값 호출
+       System.out.println(변수명1.get(2));
+
+       // [8] 리스트 요소 삭제
+       변수명1.remove(1); // (인덱스) 인덱스 요소값 삭제
+       System.out.println(변수명1);
+
+       // [9] 리스트 요소 찾기
+       System.out.println( 변수명1.indexOf("강호동")); // 값의 인덱스 반환, 없으면 -1
+       System.out.println( 변수명1.contains("강호동")); // 갑이 있을시 true, 없으면 false
+
+       // [10] 반복문과 리스트 관계
+       // 1. 일반 for문
+       for ( int inedx = 0; inedx <= 변수명1.size() -1 ; inedx++){ String str = 변수명1.get(inedx); }
+            //  인덱스 사용
+       // 2. 향상된 for 문 :  : 기준 오른쪽 리스트 내 요소 하나씩 왼쪽에 (반복)대입
+       for( String str : 변수명1 ){ } // 인덱스 활용 x
+
+       // [11] 리스트 전체 삭제
+       변수명1.clear();
+       System.out.println(변수명1); // [ ] 빈 배열 출력
+
+       // [12] 리스트 요소 여부 확인
+       System.out.println( 변수명1.isEmpty() ); // isEmpty, 리스내 요소가 하나도 없으면 true
     
+
+
+    }
 }
---
+++ src/day25_0810/java_p11.java
@@ -0,0 +1,72 @@
+package day25_0810;
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class java_p11 {
+    public static void main(String[] args) {
+    
+    // [문제 1] String 타입의 요소를 저장할 수 있는 ArrayList를 nameList 라는 이름으로 생성하세요.
+        ArrayList<String> nameList = new ArrayList<>();
+        nameList.add("유재석"); nameList.add("강호동"); nameList.add("신동엽");
+        System.out.println(nameList);
+
+    // [문제 2] 다음과 같이 초기화된 ArrayList가 있습니다.
+        ArrayList<String> fruits = new ArrayList<>();
+        fruits.add("사과"); fruits.add("바나나"); fruits.add("딸기");
+        for(int i=0; i<= fruits.size()-1 ; i++){ System.out.printf( "인덱스 %d: %s\n" , i ,fruits.get(i) );}
+
+    // [문제 3] 문제 3의 fruits 리스트를 사용합니다.
+        for(String str : fruits){System.out.println(str);}
+
+    // [문제 4] "A", "B", "C", "D", "E"를 요소로 가지는 ArrayList를 생성하세요.
+        ArrayList<String> cha = new ArrayList<>();
+        cha.add("A"); cha.add("B"); cha.add("C"); cha.add("D"); cha.add("E");
+        cha.remove(2); System.out.println(cha);
+    
+
+    // [문제 5] "자바", "파이썬", "C++"를 요소로 가지는 ArrayList를 생성하세요.
+        ArrayList<String> 문제5 = new ArrayList<>();
+        문제5.add("자바"); 문제5.add("파이썬"); 문제5.add("C++");
+        문제5.add(1,"자바스크립트");
+        System.out.println(문제5);
+
+    // [문제 6] title(문자열)과 author(문자열) 멤버 변수를 가진 Book 클래스를 만드세요.
+        Book book1 = new Book("자바", "몰라1"); Book book2 = new Book("파이썬", "몰라2"); Book book3 = new Book("C++", "몰라3");
+        ArrayList<Book> library = new ArrayList<>();
+        library.add(book1); library.add(book2); library.add(book3);  
+        for(Book book : library){System.out.println("제목: " + book.title + " 작가: "+ book.author);}
+
+    // [문제 7] Scanner를 사용하여 사용자로부터 문자열을 계속해서 입력받습니다.
+        Scanner scan = new Scanner(System.in);
+        ArrayList<String> arrayList = new ArrayList<>();
+        for(;;){
+            System.out.print("문자열 입력: "); String str = scan.next();
+            if( str.equals("종료") ){
+                System.out.println(arrayList);
+                break;
+            }else{ arrayList.add(str); }
+        }
+
+    // [문제 8] "국어", "수학", "사회", "과학"을 요소로 가지는 ArrayList를 생성하세요.
+        ArrayList<String> aList = new ArrayList<>();
+        aList.add("국어"); aList.add("수학"); aList.add("사회"); aList.add("과학");
+        aList.set(1,"영어");
+        System.out.println(aList);
+
+
+    }
+    
+}
+// [문제 6] title(문자열)과 author(문자열) 멤버 변수를 가진 Book 클래스를 만드세요.
+class Book{
+    String title; String author;
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+    
+    
+}
